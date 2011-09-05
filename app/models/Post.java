e2b2d106e8392e37d4bbfd3dc2265c34a685a5b4
Duke <taras_duk@mail.ru>
2011-09-05 22:03:17 +0300

    initial tutorial commit

---
+++ app/models/Post.java
@@ -0,0 +1,41 @@
+package models;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+import play.db.jpa.*;
+
+@Entity
+public class Post extends Model {
+
+	public String title;
+	public Date postedAt;
+
+	@Lob
+	public String content;
+
+	@ManyToOne
+	public User author;
+
+	@OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+	public List<Comment> comments;
+
+	public Post(User author, String title, String content) {
+		this.comments = new ArrayList<Comment>();
+		this.author = author;
+		this.title = title;
+		this.content = content;
+		this.postedAt = new Date();
+	}
+
+	public Comment addComment(String author, String message) {
+		Comment comment = new Comment(this, author, message);
+
+		this.comments.add(comment);
+		this.save();
+
+		return comment;
+	}
+
+}
