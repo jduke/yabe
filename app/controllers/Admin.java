5963e3c9527c22478a0c763b8664f10dc1fe98b9
Duke <taras_duk@mail.ru>
2011-10-06 23:24:18 +0300

    finished

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -1,5 +1,9 @@
 package controllers;
 
+import java.util.List;
+
+import models.Post;
+import models.Tag;
 import models.User;
 import play.mvc.Before;
 import play.mvc.Controller;
@@ -16,6 +20,47 @@ public class Admin extends Controller{
 	}
 
 	public static void index(){
-		render();
+		String user = Security.connected();
+		List<Post> posts = Post.find("author.email", user).fetch();
+		render(posts);
+	}
+	
+
+	public static void form(Long id) {
+	    if(id != null) {
+	        Post post = Post.findById(id);
+	        render(post);
+	    }
+	    render();
+	}
+	
+	public static void save(Long id, String title, String content, String tags) {
+	    Post post;
+	    if(id == null) {
+	        // Create post
+	        User author = User.find("byEmail", Security.connected()).first();
+	        post = new Post(author, title, content);
+	    } else {
+	        // Retrieve post
+	        post = Post.findById(id);
+	        // Edit
+	        post.title = title;
+	        post.content = content;
+	        post.tags.clear();
+	    }
+	    // Set tags list
+	    for(String tag : tags.split("\\s+")) {
+	        if(tag.trim().length() > 0) {
+	            post.tags.add(Tag.findOrCreateByName(tag));
+	        }
+	    }
+	    // Validate
+	    validation.valid(post);
+	    if(validation.hasErrors()) {
+	        render("@form", post);
+	    }
+	    // Save
+	    post.save();
+	    index();
 	}
 }
